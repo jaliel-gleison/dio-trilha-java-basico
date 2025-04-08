311a6276b750d0f13183c3d18f5e5f5d8fb11d51
jaliel-gleison <jalielgleison@hotmail.com>
2025-04-08 02:40:55 -0300

    alterando a classe conta terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        //Declarando as variaveis
+        double saldo = Double.valueOf(args[0]);
+        String agencia;
+        int numeroConta;
+        String nomeCorrentista;
+
+        //Criando uma instancia do Scanner para ler o teclado
+        Scanner scanner = new Scanner(System.in);
+
+            // solicitando os dados do correntista
+             System.out.println("Por favor, digite o número da Agência!");
+             agencia = scanner.nextLine();
+
+             System.out.println("Por favor, digite o número da Conta!");
+             numeroConta = scanner.nextInt();
+
+             System.out.println("Por favor, digite seu Nome!");
+             nomeCorrentista = scanner.next();
+
+                //Exibindo os dados inseridos
+                System.out.println("Olá "+ nomeCorrentista + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                +agencia+", conta "+numeroConta+ " e seu saldo "+ saldo+ " já esta disponivel para saque");
+
     }
 }
